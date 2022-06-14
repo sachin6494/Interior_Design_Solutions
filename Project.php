117a4dde90da6aaa5489d6bcebeb828b6161df34
Sachin Sharma <sachinsharma6494@gmail.com>
2022-06-14 22:10:47 +0530

    Interior_Design_Solutions

---
+++ Project.php
@@ -0,0 +1,187 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta name="viewport" content="width=device-width, intial-scale=1.0">
+            <title>Interior Design Solutions</title>
+            <link rel="stylesheet" href="assets/css/style.css"/>
+           <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+<!-- Additional CSS Files -->
+<link rel="stylesheet" href="assets/css/fontawesome.css">
+<link rel="stylesheet" href="assets/css/template.css">
+<link rel="stylesheet" href="assets/css/owl.css">
+ 
+    </head>
+    <body>
+   
+ <!-- Header -->
+ <header class="">
+      <nav class="navbar navbar-expand-lg">
+
+        <div class="container">
+          <!-- <a class="navbar-brand" href="index.php"><h2>IDS</h2></a> -->
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                      aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="about.php">About</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="blog.php">Services</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="post-details.php">Product</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact</a>
+              </li>
+              
+              <li class="nav-item">
+                <a class="nav-link" href="blog.php">Careers</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="about.php">Whychooseus</a>
+              </li>
+
+               <li class="nav-item">
+                <a class="nav-link" href="Project.php">RecentProject</a>
+              </li>
+            </ul>
+          </div>
+        </div><!-- container div-->
+      </nav>
+    </header>
+
+
+       <!--services--------------------->
+       <section class="services">
+         
+           <div class="services-heading">
+             <h1></h1><br><br><br><br><br><br>
+               <h2>Website Services Which We Provided</h2>
+               <p>This site provides a confidential, 
+                   discreet and hassle-free resolution and site provides a confidential,
+                    discreet and hassle-free resolution and discreet and
+                     hassle-free resolution and site.......</p>
+           </div>
+           <!-- box-container----------------->
+           <div class="box-container">
+               <!-- box-1 ------->
+               <div class="box">
+                   <img src="assets/images/Image5.jpg"><br>
+                   <font>Log-In to our secure website</font>
+                   <p>To begin the process , please Log-In to our secure system
+                    To begin the process , please Log-In to our secure system To begin the processplease Log-In....</p>
+                    <!--btn-------->
+                    <a href="#">Log in</a>
+               </div>
+
+                <!-- box-2 ------->
+                <div class="box">
+                    <img src="assets/images/Image5.jpg"><br>
+                    <font>View Your Website</font>
+                    <p>To begin the process , please Log-In to our secure system
+                     To begin the process , please Log-In to our secure system To begin the process please Log-In....</p>
+                     <!--btn-------->
+                     <a href="#">View</a>
+                </div>
+
+                 <!-- box-3 ------->
+            <div class="box">
+                <img src="assets/images/Image5.jpg"><br>
+                <font>Select Your Plan</font>
+                <p>To begin the process , please Log-In to our secure system
+                 To begin the process , please Log-In to our secure system To begin the process please Log-In....</p>
+                 <!--btn-------->
+                 <a href="#">Select Plan</a>
+            </div>
+                 <!-- box-4 ------->
+           <div class="box">
+                <img src="assets/images/Image5.jpg"><br>
+                <font>Login in secure website</font>
+                <p>To begin the process , please Log-In to our secure system
+                 To begin the process , please Log-In to our secure system To begin the process please Log-In....</p>
+                 <!--btn-------->
+                 <a href="#">Log in</a>
+            </div>
+
+             <!-- box-5 ------->
+             <div class="box">
+                <img src="assets/images/Image5.jpg"><br>
+                <font>Confrim Your Websites</font>
+                <p>To begin the process , please Log-In to our secure system
+                 To begin the process , please Log-In to our secure system To begin the process please Log-In....</p>
+                 <!--btn-------->
+                 <a href="#">View</a>
+            </div>
+
+           </div>
+       </section>
+
+       <!--footer------->
+       <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <ul class="social-icons">
+              <li><a href="#">Facebook</a></li>
+              <li><a href="#">Twitter</a></li>
+              <li><a href="#">Youtube</a></li>
+              <li><a href="#">Linkedin</a></li>
+              <li><a href="#">Dribbble</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-12">
+            <div class="copyright-text">
+              <p>Copyright 2022 Interior 
+
+                 | Design: <a rel="nofollow" href="#" target="_parent"> Solutions</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Additional Scripts -->
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/owl.js"></script>
+    <script src="assets/js/slick.js"></script>
+    <script src="assets/js/isotope.js"></script>
+    <script src="assets/js/accordions.js"></script>
+
+    <script language = "text/Javascript">
+      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+      function clearField(t){                   //declaring the array outside of the
+      if(! cleared[t.id]){                      // function makes it static and global
+          cleared[t.id] = 1;  // you could use true and false, but that's more typing
+          t.value='';         // with more chance of typos
+          t.style.color='#fff';
+          }
+      }
+    </script>
+    </body>
+</html>
